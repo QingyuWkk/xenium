28ebb19c746be88f10eaece67b3c198194e52ffd
QingyuWkk <qingyuwang.qw@outlook.com>
2024-04-03 09:44:45 -0500

    Update xenium.R
    add function "cell_area_and_cluster()" to process Xenium objects and give plot outputs

--- xenium.R
+++ xenium.R
@@ -1,5 +1,5 @@
 library(Seurat)
-library(terra)
+library(ggplot2)
 
 path <- "C:/Users/AlainaKK/Downloads/Xenium/output-XETG00074__0010499__IC071922__20231001__221931"
 # Load the Xenium data
@@ -13,44 +13,47 @@ xenium_obj <- subset(xenium_obj, subset = nCount_Xenium > 0)
 # Molecule pixel coordinates (“microns”): Gives the pixel coordinates of individual molecules.
 xenium_read <- ReadXenium(path, outs = c("matrix", "microns"), type = "centroids", mols.qv.threshold = 20)
 
+# Run UMAP to reduce dimensions for visualization
+xenium_obj <- SCTransform(xenium_obj, assay = "Xenium")
+xenium_obj <- RunPCA(xenium_obj, npcs = 30, features = rownames(xenium_obj))
+xenium_obj <- RunUMAP(xenium_obj, dims = 1:30)
+
+# Identify clusters of cells
+xenium_obj <- FindNeighbors(xenium_obj, reduction = "pca", dims = 1:30)
+xenium_obj <- FindClusters(xenium_obj, resolution = 0.3)
 
 # Function to compute each cell's area and correlate it with clusters
-get_cell_area_and_correlate <- function(xenium_obj) {
+cell_area_and_cluster <- function(xenium_obj, cluster = "seurat_clusters",plot_options= "Violin", add_p_val = F,exc_cell_type = F) {
+  # This function gets processed Xenium objects and give several plot outputs
   
-  xenium_obj[['area']] <- paste0("(",xenium_read[['centroids']][['x']],",",xenium_read[['centroids']][['y']],")")
+  # get area for each cell
+  xenium_obj[['cell_area']] <- sapply(xenium_obj@images[["fov"]]@boundaries[["segmentation"]]@polygons,function(x) x@area)
   
-  # Run UMAP to reduce dimensions for visualization
-  xenium_obj <- SCTransform(xenium_obj, assay = "Xenium")
-  xenium_obj <- RunPCA(xenium_obj, npcs = 30, features = rownames(xenium_obj))
-  xenium_obj <- RunUMAP(xenium_obj, dims = 1:30)
+  # one can change a cell_type or another metadata column data with seurat_clusters
+  ## Does it mean to add annotation for the features or to add new clustering method??
+  xenium_obj@meta.data[["seurat_clusters"]] <- cluster
   
-  # Identify clusters of cells
-  xenium_obj <- FindNeighbors(xenium_obj, reduction = "pca", dims = 1:30)
-  xenium_obj <- FindClusters(xenium_obj, resolution = 0.3)
+  # We have violin, boxplot, violin + boxplot options
+  if (plot_options == "Violin") {
+    ggplot2::ggplot(xenium_obj@meta.data, mapping=aes(x=seurat_clusters,y=cell_area,fill=seurat_clusters)) + geom_violin(trim=FALSE)
+  } else if (plot_options == "Boxplot") {
+    ggplot2::ggplot(xenium_obj@meta.data, mapping=aes(x=seurat_clusters,y=cell_area,fill=seurat_clusters)) + geom_boxplot(width=0.2,position = position_dodge(0.9))
+  } else if (plot_options == "ViolinBoxplot") {
+    ggplot2::ggplot(xenium_obj@meta.data, mapping=aes(x=seurat_clusters,y=cell_area,fill=seurat_clusters)) + geom_violin(trim=FALSE,color="white") + geom_boxplot(width=0.1,position = position_dodge(0.9))
+  }
   
-  # Plot UMAP, colored by cell area ???
-  # Scatter plot
-  FeaturePlot(xenium_obj, features = rownames(xenium_obj))
-  # Violin plot
-  VlnPlot(xenium_obj, features = rownames(xenium_obj))
+  # If add_p_val is True add pairwise comparison
+  if (add_p_val) {
+    pairwise.t.test(x = xenium_obj@meta.data$cell_area, g = xenium_obj@meta.data$seurat_clusters, p.adjust.method = "BH")
+  }
   
-  # Plot UMAP, colored by clusters
-  DimPlot(xenium_obj)
-  
-  # correlation ???
-  ClusterAreaCorr <- cor.test(xenium_obj[['area']], xenium_obj[['seurat_clusters']], method = "spearman")
+  # One can exclude several clusters / Cell Types by using exc_cell_type
+  if (exc_cell_type != FALSE) {
+    xenium_obj@meta.data <- subset(xenium_obj@meta.data, subset = !(seurat_clusters %in% exc_cell_type))
+  }
   
   # Return the modified Seurat object, which now includes cell area metadata
-  # and clustering information
   return(xenium_obj)
 }
-
-xenium_seurat <- get_cell_area_and_correlate(xenium_obj)
-
-
-
-
-
-
 
 
